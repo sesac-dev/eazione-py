4d10a36f53a1510359a483498a8fb1a06ef8d123
Henry-Cha <sungwon4728@naver.com>
2024-07-17 10:36:49 +0900

    feat: json,file 같이 파라미터로 받기

--- app/domain/router.py
+++ app/domain/router.py
@@ -3,14 +3,26 @@ from domain.request_models import *
 from fastapi.responses import StreamingResponse
 from domain.docs_fill import draw_text_on_image
 from domain.match_item import match
+from pydantic import BaseModel, ValidationError
+from fastapi.exceptions import HTTPException
+from fastapi.encoders import jsonable_encoder
 
 
 router = APIRouter(
     prefix="/ai",
 )
 
+def checker(data: str = Form(...)):
+    try:
+        return DocsFillRequest.model_validate_json(data)
+    except ValidationError as e:
+        raise HTTPException(
+            detail=jsonable_encoder(e.errors()),
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        )
+
 @router.post("/docsfill")
-async def docs_fill(image: UploadFile = File(...)) :
+async def docs_fill(image: UploadFile = File(...), data: DocsFillRequest = Depends(checker)) :
     data_dict = match()
     image_stream = await image.read()
     img_byte_arr = draw_text_on_image(image_stream, data_dict)
