91cc31e614aa0ab68b949d06d002a63862277311
Henry-Cha <sungwon4728@naver.com>
2024-08-01 17:25:27 +0900

    fix

--- app/domain/router.py
+++ app/domain/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, Form, File, Body,Depends,status
+from fastapi import APIRouter, UploadFile, Form, File, Body,Depends,status,Body
 from domain.models import *
 from fastapi.responses import StreamingResponse
 from domain.docs_fill import draw_text_on_image
@@ -14,7 +14,7 @@ router = APIRouter(
 
 def checker(data: str = Form(...)):
     try:
-        return DocsFillRequest.parse_raw(data)  # JSON 데이터를 DocsFillRequest 모델로 파싱
+        return DocsFillRequest(**json.loads(data))  # JSON 데이터를 DocsFillRequest 모델로 파싱
     except ValidationError as e:
         raise HTTPException(
             detail=jsonable_encoder(e.errors()),
@@ -23,11 +23,17 @@ def checker(data: str = Form(...)):
 
 @router.post("/docsfill")
 async def docs_fill(image: UploadFile = File(...), data: DocsFillRequest = Depends(checker)) :
-    filled_empty_items = await match(data)
-    print(filled_empty_items)
-    print("-------------------------------------")
-    translate_items = [] #await translate(data)
-    print(translate_items)
-    image_stream = await image.read()
-    img_byte_arr = draw_text_on_image(image_stream, filled_empty_items,translate_items)
-    return StreamingResponse(img_byte_arr, media_type="image/jpeg")
+    try:
+        filled_empty_items = await match(data)
+        print(filled_empty_items)
+        print("-------------------------------------")
+        translate_items = [] #await translate(data)
+        print(translate_items)
+        image_stream = await image.read()
+        img_byte_arr = draw_text_on_image(image_stream, filled_empty_items,translate_items)
+        return StreamingResponse(img_byte_arr, media_type="image/jpeg")
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail=jsonable_encoder(e.errors())
+        )    
